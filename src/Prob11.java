3df50cfacd44f97a8750947ffb4145b9960d21d2
lhf-leo <hanfuliao@gmail.com>
2015-01-13 22:51:29 -0500

    change gitignore

--- src/Prob11.java
+++ src/Prob11.java
@@ -25,7 +25,6 @@ The product of these numbers is 26 × 63 × 78 × 14 = 1788696.
 
 What is the greatest product of four adjacent numbers in the same direction
 (up, down, left, right, or diagonally) in the 20×20 grid?
- * @author hanfu
  *
  */
 public class Prob11 {
